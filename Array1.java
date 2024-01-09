2edf579160603d59a4255f71b78e48f9e0b5af11
balajibalz <155128782+balajibalz@users.noreply.github.com>
2024-01-09 15:55:30 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,12 @@
+class Array1                                                                    
+{
+	public static void main(String[] args) 
+	{
+		float a[]={34.3f,67.2f,38.6f};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+		System.out.print(a[i]+" ");
+		}
+	}
+}
